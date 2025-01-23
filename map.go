047e94ab0ac86f3cd1b69db40d7ee20dd58855b2
AlexseyWeb <alongenet32@gmail.com>
2025-01-23 22:10:41 +0300

    added structure a map

---
+++ map.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func printMap(person map[string]string) {
+	for k, v := range person {
+		fmt.Println("Key -> ", k, " Value -> ", v)
+	}
+	fmt.Println("----------------------------")
+}
+
+func main() {
+	person := make(map[string]string)
+
+	person["name"] = "Alexsey"
+	person["age"] = "33"
+	person["sallary"] = "30_000"
+
+	printMap(person)
+
+	html_tag := make(map[string]string)
+	html_tag["h1"] = "Header"
+	html_tag["h2"] = "Header 2"
+	html_tag["body"] = "Body"
+
+	printMap(html_tag)
+}
